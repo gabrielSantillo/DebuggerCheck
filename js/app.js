ea7b1fe49528f2915819291fd2f07a4a56ad7558
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2022-07-07 19:30:35 -0600

    Closing my VSCODE to see if my server port goes back to 5500

---
+++ js/app.js
@@ -0,0 +1,11 @@
+let username = "Gabriel";
+let user_age = 26;
+let is_member = true;
+
+if (username === "admin") {
+    console.log("You are very welcome here.");
+} else if (user_age >= 30 && is_member === true) {
+    console.log("You are very welcome here.");
+} else {
+    console.log("Welcome");
+}
