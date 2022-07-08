d0fff0be9167551b40ecdf54a9f85ad0d8c3d21d
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2022-07-07 20:22:59 -0600

    addind numbers

--- js/app.js
+++ js/app.js
@@ -12,3 +12,9 @@ if (username === "admin") {
 
 console.log(`Hello ${username}`);
 console.log(`You are: ${user_age}`);
+
+let max_volume = 1523;
+let current_volume = 426.57896;
+
+console.log(max_volume / current_volume);
+console.log((max_volume / current_volume) * 100);
